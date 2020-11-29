3a8ec3cabc45446dfe3e0a52b002bf445b0950d6
高田育実 <ikumi@takadaikuminoMacBook-Pro.local>
2020-11-29 14:11:54 +0900

    '電話番号正規表現　ハイフン取り消し機能追加'

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let tel ;
+let afterTel;
+
+    function Henkan() {
+        tel = document.getElementById('tel').value;
+        afterTel = tel.replaceAll('-','');
+        console.log(afterTel);
+    }
